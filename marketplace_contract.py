b13fc2109ab44710cd9a3a2fbe924948a3b2217a
zeredogo <zeredogo@gmail.com>
2022-10-14 00:13:48 +0100

    change made

--- marketplace_contract.py
+++ marketplace_contract.py
@@ -1,3 +1,4 @@
+from ast import Global
 from pyteal import *
 
 
@@ -28,4 +29,38 @@ class Product:
         ])
 
     
+    def buy(self):
+        count = Txn.application_args[1]
+        valid_number_of_transactions = Global.group_size() == Int(2)
+
+
+        valid_payment_to_seller = And(
+            Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[1].receiver() == Global.creator_address(),
+            Gtxn[1].amount() == App.globalGet(self.Variables.price) * Btoi(count),
+            Gtxn[1].sender() == Gtxn[0].sender(),
+        )
+
+        can_buy = And(valid_number_of_transactions,
+                      valid_payment_to_seller)
+
+        
+        update_state = Seq([
+            App.globalPut(self.Variables.sold, App.globalGet(self.Variables.sold) + Btoi(count)),
+            Approve()
+        ])
+
+
+        return If(can_buy).Then(update_state).Else(Reject())
+
     
+    def application_deletion(self):
+        return Return(Txn.sender() == Global.creator_address())
+
+    
+    def application_start(self):
+        return Cond(
+            [Txn.application_id() == Int(0), self.application_creation()],
+            [Txn.on_completion() == OnComplete.DeleteApplication, self.application_deletion()],
+            [Txn.application_args[0] == self.AppMethods.buy, self.buy()]
+        )
